f5b9834514f8102bb81d14d66161d213284c6c5d
HadarRoze <hadarroze@utexas.edu>
2019-02-05 18:20:51 -0600

    Hadar Driving
    Lorrie's end of driving-18:20

--- app/src/main/java/com/example/tria_rozenberg_hw2/MainActivity.java
+++ app/src/main/java/com/example/tria_rozenberg_hw2/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.tria_rozenberg_hw2;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 // keys and addresses
     //google request format: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
     final String googleAddress = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-    final String googleKey = "AIzaSyCNND_DqC1ODB6J5a5K3W_IYQRWJAL9Qz4";
+    final String googleKey = "&key=AIzaSyCNND_DqC1ODB6J5a5K3W_IYQRWJAL9Qz4";
     final String darkSkyAddress = "https://api.darksky.net/forecast/fb4ffcd5b6e8f190449448adc636025a/";
+    public JSONArray rspns;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +41,56 @@ public class MainActivity extends AppCompatActivity {
     // 4. Display the weather conditions (temperature, humidity, wind speed, precipitation)
     // 5. Display on map
     public void onSubmit(View view) {
-
+        String address = getAddress();
+        Log.i("tag","Passed get address");
+        TextView txtv = (TextView) findViewById(R.id.textView);
+        txtv.setText(googleAddress+address+googleKey);
+        callGoogle(googleAddress+address+googleKey);
+        //readCoords();
+        //TextView txtv = (TextView) findViewById(R.id.textView);
+        //txtv.setText(rspns.toString());
     }
 
-    
+    //Hadar driving
     public String getAddress(){
         EditText inpt = (EditText) findViewById(R.id.editText);
+        String address = inpt.getText().toString();
+        address = address.replace(" ", "+");
+        return address;
+    }
 
+    public void callGoogle(final String address){
+        Log.i("tag","Got to call google");
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, address, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.i("tag","Got response");
+                TextView txtv = (TextView) findViewById(R.id.textView);
+                txtv.setText(address);
+                //rspns = response;
+            }
+        }, new Response.ErrorListener(){
+                @Override
+                public void onErrorResponse(VolleyError error) {
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
 
-
+    public void readCoords(){
+        TextView txtv = (TextView) findViewById(R.id.textView);
+        JSONObject temp = null;
+        String lng = null;
+        String lat = null;
+        /*try {
+            temp = rspns.getJSONObject("geometry").getJSONObject("location");
+            lng = Double.toString(temp.getDouble("lng"));
+            lat = Double.toString(temp.getDouble("lat"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+        txtv.setText(lng+" "+lat);
     }
 
     }
